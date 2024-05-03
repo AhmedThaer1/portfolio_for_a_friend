777f6bfe6426a933f38d91a6d5dfeec3ef386b81
AhmedThaer1 <110353919+AhmedThaer1@users.noreply.github.com>
2024-05-03 17:54:21 +0300

    adding famerMotion and appling it to the navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,9 +4,18 @@ import Link from "next/link";
 import { NavLinks } from "../_links/NavLinks";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
+import {
+  listVariants,
+  topVartiants,
+  centerVartiants,
+  bottomVartiants,
+  listItemsVariants,
+} from "@/framerMotion/variants";
 
 interface MenuButtonProps {
   setIsOpen: (value: boolean | ((prev: boolean) => boolean)) => void;
+  isOpen: boolean;
 }
 
 export default function Navbar() {
@@ -42,30 +51,52 @@ export default function Navbar() {
       </div>
 
       <div className="md:hidden">
-        <MenuButton setIsOpen={setIsOpen} />
+        <MenuButton setIsOpen={setIsOpen} isOpen={isOpen} />
+
         {isOpen && (
-          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+          <motion.div
+            variants={listVariants}
+            initial={"closed"}
+            animate={"opened"}
+            className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40"
+          >
             {NavLinks.map((link) => (
-              <Link href={link.url} key={link.url}>
-                {link.title}
-              </Link>
+              <motion.div
+                className=""
+                variants={listItemsVariants}
+                key={link.url}
+              >
+                <Link href={link.url}>{link.title}</Link>
+              </motion.div>
             ))}
-          </div>
+          </motion.div>
         )}
       </div>
     </div>
   );
 }
 
-function MenuButton({ setIsOpen }: MenuButtonProps) {
+function MenuButton({ setIsOpen, isOpen }: MenuButtonProps) {
   return (
     <button
       className="w-10 h-8 flex flex-col justify-between z-50 relative"
       onClick={() => setIsOpen((prev) => !prev)}
     >
-      <div className="w-10 h-1 bg-black rounded"></div>
-      <div className="w-10 h-1 bg-black rounded"></div>
-      <div className="w-10 h-1 bg-black rounded"></div>
+      <motion.div
+        variants={topVartiants}
+        animate={isOpen ? "opened" : "closed"}
+        className="w-10 h-1 bg-black rounded origin-left"
+      ></motion.div>
+      <motion.div
+        variants={centerVartiants}
+        animate={isOpen ? "opened" : "closed"}
+        className="w-10 h-1 bg-black rounded"
+      ></motion.div>
+      <motion.div
+        variants={bottomVartiants}
+        animate={isOpen ? "opened" : "closed"}
+        className="w-10 h-1 bg-black rounded origin-left"
+      ></motion.div>
     </button>
   );
 }
